b5752e24fcfa10ce54725a9938674230bcd3c14c
kevinhuanca <kevinhuanca75@gmail.com>
2024-12-02 12:45:39 -0300

    Add map

--- app/src/main/java/com/ulp/canchas/ui/inicio/InicioViewModel.java
+++ app/src/main/java/com/ulp/canchas/ui/inicio/InicioViewModel.java
@@ -1,7 +1,50 @@
 package com.ulp.canchas.ui.inicio;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+import android.content.Context;
 
-public class InicioViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class InicioViewModel extends AndroidViewModel {
+
+    private Context context;
+    private MutableLiveData<MapaActual> mapaActual;
+
+    public InicioViewModel(@NonNull Application application) {
+        super(application);
+        context = application.getApplicationContext();
+    }
+
+    public LiveData<MapaActual> getMapaActual() {
+        if (mapaActual == null) {
+            mapaActual = new MutableLiveData<>();
+        }
+        return mapaActual;
+    }
+
+    public void obtenerMapa() {
+        MapaActual mp = new MapaActual();
+        mapaActual.setValue(mp);
+    }
+
+    public class MapaActual implements OnMapReadyCallback {
+        LatLng COMPLEJO_DEPORTIVO = new LatLng(-33.150720, -66.306864);
+
+        @Override
+        public void onMapReady(@NonNull GoogleMap googleMap) {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            googleMap.addMarker(new MarkerOptions().position(COMPLEJO_DEPORTIVO).title("Complejo Deportivo"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(COMPLEJO_DEPORTIVO, 18));
+        }
+    }
+
 }
